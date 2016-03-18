5ee6d5ff4136fd9942af8d648e302d9ed4e035d2
joss1997 <jocyroman@gmail.com>
2016-03-18 16:07:18 -0600

    Separando archivos

--- alien.c
+++ alien.c
@@ -1,38 +1,4 @@
-#include<stdio.h>  
-#include <string.h> 
-#include <stdlib.h>
-
-typedef struct alien_t{
-
-	char nombre[30];
-	int id;
-	char especie;
-	struct planeta_t * planetas;
-	struct idiomas_t * idiomas;
-	struct alien_t * siguiente;
-	
-	} alien;
-	
-	
-typedef struct planeta_t{
-
-	char nombre[30];
-	int starwars_code;
-	char agua;
-	char oxigeno;
-	struct planeta_t * siguiente;
-	
-	} planeta;
-	
-typedef struct idiomas_t{
-
-	char nombre[30];
-	char tipo;
-	int simbolos;
-	struct idiomas_t * siguiente;
-	struct planeta_t * planetas;
-
-	} idiomas;
+#include "struct.h"
 	
 int main(){
 	
