1204b1dc842ec2d8dcd8e0bd21a638e9ac34da5e
joss1997 <jocyroman@gmail.com>
2016-03-18 15:49:07 -0600

    Incluyendo archivo c

---
+++ alien.c
@@ -0,0 +1,97 @@
+#include<stdio.h>  
+#include <string.h> 
+#include <stdlib.h>
+
+typedef struct alien_t{
+
+	char nombre[30];
+	int id;
+	char especie;
+	struct planeta_t * planetas;
+	struct idiomas_t * idiomas;
+	struct alien_t * siguiente;
+	
+	} alien;
+	
+	
+typedef struct planeta_t{
+
+	char nombre[30];
+	int starwars_code;
+	char agua;
+	char oxigeno;
+	struct planeta_t * siguiente;
+	
+	} planeta;
+	
+typedef struct idiomas_t{
+
+	char nombre[30];
+	char tipo;
+	int simbolos;
+	struct idiomas_t * siguiente;
+	struct planeta_t * planetas;
+
+	} idiomas;
+	
+int main(){
+	
+	alien * lista_aliens;
+	lista_aliens = malloc(sizeof(alien));
+	strcpy(lista_aliens->nombre, "Alpha");
+	lista_aliens->id = 1234;
+	lista_aliens->especie = 'e';
+	
+	planeta * p;
+	p = malloc(sizeof(planeta));
+	
+	lista_aliens->planetas = p;
+	strcpy(lista_aliens->planetas->nombre , "ALPATH");
+	lista_aliens->planetas->starwars_code = 23;
+	lista_aliens->planetas->agua = 'n';
+	lista_aliens->planetas->oxigeno = 's';
+	lista_aliens->planetas->siguiente = NULL;
+	
+	idiomas * i;
+	i = malloc(sizeof(idiomas));
+	lista_aliens->idiomas = i;
+	strcpy(lista_aliens->idiomas->nombre , "AL");
+	lista_aliens->idiomas->tipo = 'a';
+	lista_aliens->idiomas->simbolos = 67;
+	lista_aliens->idiomas->siguiente = NULL;
+	lista_aliens->idiomas->planetas = p;
+	printf("Alien:\n ");  
+	printf("Nombre:\n ");  
+	    
+	printf("%s\n",lista_aliens->nombre);
+	printf("Id:\n "); 
+	printf("%d\n",lista_aliens->id);
+	printf("Especie:\n "); 
+	printf("%c\n",lista_aliens->especie);
+	printf("Planetas:\n "); 
+	printf("Nombre:\n "); 
+	
+	printf("%s\n",lista_aliens->planetas->nombre);
+	printf("Starwars_code:\n "); 
+	printf("%d\n",lista_aliens->planetas->starwars_code);
+	printf("Agua:\n "); 
+	printf("%c\n",lista_aliens->planetas->agua);
+	printf("Oxigeno:\n "); 
+	printf("%c\n",lista_aliens->planetas->oxigeno);
+	printf("Idiomas:\n "); 
+	printf("Nombre:\n "); 
+	printf("%s\n",lista_aliens->idiomas->nombre);
+	printf("Tipo:\n "); 
+	printf("%c\n",lista_aliens->idiomas->tipo);
+	printf("Simbolos:\n "); 
+	printf("%d\n",lista_aliens->idiomas->simbolos);
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
